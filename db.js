04f915c6d3be5660af0b5eb0017ba2d0f1cba3d2
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-10-01 20:03:36 -0700

    complete step 1

--- db.js
+++ db.js
@@ -1,2 +1,24 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+// ==================================================
+
+let DB_URI;
+
+switch (process.env.NODE_ENV) {
+  case 'test':
+    DB_URI = 'postgresql://postgres@localhost/biztime_test';
+    break;
+
+  default:
+    DB_URI = 'postgresql://postgres@localhost/biztime';
+}
+
+const db = new Client({ connectionString: DB_URI });
+
+db.connect();
+
+// ==================================================
+
+module.exports = db;
